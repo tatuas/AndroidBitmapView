7f78afe54512160b5e6123a63070bb0adf39d041
Tatsuya Sawai <a209050@gmail.com>
2013-12-24 02:26:33 +0900

    Frist commit.

---
+++ src/com/tatuas/android/bitmapview/sample/FromJavaActivity.java
@@ -0,0 +1,32 @@
+package com.tatuas.android.bitmapview.sample;
+
+import java.io.File;
+
+import com.tatuas.android.bitmapview.BitmapView;
+import com.tatuas.android.bitmapview.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.LinearLayout;
+
+public class FromJavaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java);
+        BitmapView bitmapView = new BitmapView(this);
+        bitmapView.setImageFromFile(new File("/storage/emulated/0/20131223064159.jpg"), 600, 400);
+        this.addContentView(bitmapView, new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sub, menu);
+        return true;
+    }
+
+}
---
+++ src/com/tatuas/android/bitmapview/sample/FromXmlActivity.java
@@ -0,0 +1,35 @@
+package com.tatuas.android.bitmapview.sample;
+
+import com.tatuas.android.bitmapview.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class FromXmlActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xml);
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), FromJavaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
